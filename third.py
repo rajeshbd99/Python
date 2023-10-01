3ce82c75cf791512afdc5f042989f99ac2a16b20
rajeshbd99 <rajesh99.bd@gmail.com>
2023-10-01 18:46:46 +0530

    Variables and data types

---
+++ third.py
@@ -0,0 +1,18 @@
+a = 1
+b = True
+c = "Harry"
+d = None
+
+print("Type of a is : ",type(a))
+print("Type of b is : ",type(b))
+print("Type of c is : ",type(c))
+print("Type of d is : ",type(d))
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+print(dict1)
