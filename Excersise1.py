300ce1e0bd42e8508cba527a3d626cd95d4452be
rajeshbd99 <rajesh99.bd@gmail.com>
2023-10-01 20:30:58 +0530

    Calculator

---
+++ Excersise1.py
@@ -0,0 +1,20 @@
+a=int(input("Enter the value of a : "))
+b=int(input("Enter the value of b : "))
+
+add = a+b
+sub = a-b
+mul = a*b
+div = a/b
+mod = a%b
+exp = a**b
+floor = a//b
+
+print("Addition of a and b is : ",add)
+print("Substraction of a and b is : ",sub)
+print("Multiplication of a and b is : ",mul)
+print("Division of a and b is : ",div)
+print("Modulus of a and b is : ",mod)
+print("Exponent of a and b is : ",exp)
+print("Floor Division of a and b is : ",floor)
+
+
