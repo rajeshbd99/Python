f0f0456e18c0849c4cd9909dff293964c2011a29
rajeshbd99 <rajesh99.bd@gmail.com>
2023-10-01 18:13:36 +0530

    comment,space sequence,print statement

---
+++ second.py
@@ -0,0 +1,11 @@
+"""
+This is the second module
+three dots are used to indicate comments that span multiple lines
+we can also uss single quotes to indicate comments that span multiple lines
+"""
+#single line comment
+
+print("Hello World from today i started learning python\"100days of code\"\ni am very excited to learn python")
+
+print("Hello World ", 9, 7 , sep="* ", end=" &\n")
+print("Learning Python is very easy")
