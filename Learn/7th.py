d86b92e1bab392132804c3ad10428f2aa11dfdee
rajeshbd99 <rajesh99.bd@gmail.com>
2023-10-03 02:16:28 +0530

    if,else,elif

---
+++ Learn/7th.py
@@ -0,0 +1,13 @@
+#if,else, else if statements
+
+age = int(input("Enter your age : "))
+print("Your age is : ",age)
+
+if(age>18):
+    print("You are eligible for voting")
+elif(age==18):
+    print("congratulations! This yearis your first time to vote")
+else:
+    print("You are not eligible for voting")
+        
+ 
