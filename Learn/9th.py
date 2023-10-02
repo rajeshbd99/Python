c1c39db0428243f2761061a1a28025f1e88609ea
rajeshbd99 <rajesh99.bd@gmail.com>
2023-10-03 02:59:47 +0530

    for loop

---
+++ Learn/9th.py
@@ -0,0 +1,16 @@
+#for loop
+
+for i in range(1,101):
+    print (i)
+
+
+colors= ["red","green","blue","yellow"]
+
+for color in colors :
+    print(color)
+    for i in color:
+     print(i)
+     
+for k in range(0,21,5):
+    print(k)
+    
