0fb674a5d0a2866395623be55f2a6b3bb18f39a1
rajeshbd99 <rajesh99.bd@gmail.com>
2023-10-03 20:29:46 +0530

    while loop

---
+++ Learn/10th.py
@@ -0,0 +1,15 @@
+#while loop
+
+i=0
+while (i<=3):
+    print(i)
+    i=i+1
+print("Done with the loop")
+
+count = 5
+while(count>0):
+    print(count)
+    count=count-1
+else:
+    print("Done with the loop")
+    
