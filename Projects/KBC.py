e8b6d4049769bb37069c7ce8f52399d2e430e231
rajeshbd99 <rajesh99.bd@gmail.com>
2023-10-05 22:35:24 +0530

    KBC

---
+++ Projects/KBC.py
@@ -0,0 +1,7 @@
+#create a program capable of playing the game of "Who Wants to Be a Millionaire?"
+#The program should have a set of 15 questions, each worth an increasing amount of money
+#use list data type to store the questions and their correct answer
+#dispaly the final amount the person is taking after home
+
+
+
